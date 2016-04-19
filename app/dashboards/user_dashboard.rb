b4f09023a022f1d56865871bf78e2259e5409e02
Adam Becker <ad@mbecker.cc>
2016-04-18 22:33:48 -0400

    sidebar layout

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -25,7 +25,7 @@ class UserDashboard < Administrate::BaseDashboard
     confirmed_at: Field::DateTime,
     confirmation_sent_at: Field::DateTime,
     unconfirmed_email: Field::String,
-    permission_level: Field::String,
+    permission_level: Field::Number,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }
