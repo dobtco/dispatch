cc9aecbb0ae21879b220d1c3d1fdddb60bd254da
Adam Becker <ad@mbecker.cc>
2016-04-20 17:49:07 -0400

    sign in after confirming email

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,4 +1,12 @@
 module Users
   class ConfirmationsController < Devise::ConfirmationsController
+    # Automatically sign in after confirming email address.
+    def show
+      super do
+        if resource.errors.empty?
+          sign_in(resource)
+        end
+      end
+    end
   end
 end
