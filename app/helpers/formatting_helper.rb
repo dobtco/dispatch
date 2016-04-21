3ccc62e5d33d93931bf77e853110102e1ab8be5b
Adam Becker <ad@mbecker.cc>
2016-04-21 10:50:51 -0400

    ocd

--- app/helpers/formatting_helper.rb
+++ app/helpers/formatting_helper.rb
@@ -6,10 +6,10 @@ module FormattingHelper
   def simpler_format(text)
     text = '' if text.nil?
     text = text.dup
-    text = CGI::escapeHTML(text)
+    text = CGI.escapeHTML(text)
     text = text.to_str
-    text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
-    text.gsub!(/\n\n+/, "<br /><br />")  # 2+ newline  -> br br
+    text.gsub!(/\r\n?/, "\n") # \r\n and \r -> \n
+    text.gsub!(/\n\n+/, '<br /><br />') # 2+ newline  -> br br
     text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
     text.html_safe
   end
