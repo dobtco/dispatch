d612dceb504d5d9e5c8e7495ce7a356baa5a4e0d
Adam Becker <ad@mbecker.cc>
2016-04-21 17:30:40 -0400

    override "theme" css

--- config/initializers/theme.rb
+++ config/initializers/theme.rb
@@ -3,3 +3,5 @@ def require_if_exists(path)
 end
 
 require_if_exists BeaconConfiguration.theme_path.join('simple_form.rb')
+
+Rails.configuration.assets.paths += Dir[BeaconConfiguration.theme_path.join("assets/**/*")]
