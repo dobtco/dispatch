ef1a227d01063bfb41781794c77b6e3418da61d2
Adam Becker <ad@mbecker.cc>
2016-04-22 00:09:20 -0400

    try to make sure none of the views look like shit

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,12 +5,12 @@ describe 'Signing in' do
 
   it 'works properly' do
     visit root_path
-    click_link 'Sign in'
+    click_link t('sign_in')
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password'
-    click_button 'Log in'
+    click_button t('sign_in')
     expect(page.body).to include t('devise.sessions.signed_in')
-    click_link 'Sign out'
+    click_link t('sign_out')
     expect(page.body).to include t('devise.sessions.signed_out')
   end
 end
