84bfcbdee37c95dc00c5be5a6b3af055b940088f
Adam Becker <ad@mbecker.cc>
2016-04-21 20:08:24 -0400

    don't show empty sidebar boxes

--- lib/submission_adapters/none.rb
+++ lib/submission_adapters/none.rb
@@ -1,5 +1,11 @@
 module SubmissionAdapters
   class None < SubmissionAdapters::Base
     self.name = 'None'
+
+    def submit_proposals_instructions
+      %(
+        Look at the opportunity description for details on submitting a proposal.
+      )
+    end
   end
 end
