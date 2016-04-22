814b0a7b7e1b2229be03d32f46e1f340107feb8c
Adam Becker <ad@mbecker.cc>
2016-04-22 10:27:18 -0400

    shitty selectize monkeypatch

--- themes/dvl-core/assets/javascripts/theme.js
+++ themes/dvl-core/assets/javascripts/theme.js
@@ -6,6 +6,15 @@
 
 var Dvl = {};
 
+// Monkeypatch Selectize to not use "item" class, which conlflits with dvl-core...
+var oldSetupTemplates = Selectize.prototype.setupTemplates;
+Selectize.prototype.setupTemplates = function(){
+  oldSetupTemplates.apply(this, arguments);
+  this.settings.render.item = function(data, escape) {
+    return '<div class="selectize-item">' + escape(data[this.settings.labelField]) + '</div>';
+  }
+}
+
 $(function(){
   $('body').styledSelect()
   $('body').styledControls()
