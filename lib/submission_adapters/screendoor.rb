2c54fa183fef8ba6f7efe9d239957745176aa337
Adam Becker <ad@mbecker.cc>
2016-04-27 09:25:48 -0400

    make "review submissions" link open in a new window

--- lib/submission_adapters/screendoor.rb
+++ lib/submission_adapters/screendoor.rb
@@ -9,8 +9,7 @@ module SubmissionAdapters
     end
 
     def view_proposals_link_text
-      "Review submissions&nbsp;
-      <i class='fa fa-external-link'></i>".html_safe
+      "Review submissions <i class='fa fa-external-link'></i>".html_safe
     end
 
     def submit_proposals_instructions
